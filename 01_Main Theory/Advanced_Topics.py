1f12ee49b514ba41473d2cc79ef1043fe6ed7639
Kamal Rathi <kamalrathi087@gmail.com>
2026-07-05 08:40:05 +0530

    Advanced Topics

---
+++ 01_Main Theory/Advanced_Topics.py
@@ -0,0 +1,32 @@
+# #Decorators
+
+# def extragreetings(func):
+#     def wrapper():
+#         print("Hello from NYC team")
+#         func()
+#         print("Thankyou visit again")
+#     return wrapper
+
+# @extragreetings
+# def greetings():
+#     print("Good Morning")
+
+# greetings()
+
+
+# # *args and **kwargs
+# def addition(*args):
+#     s = 0
+#     for i in args:
+#         s += i
+#     return s
+
+# print(addition(1,23,4444444444,6,7,4243,87635,24323,435,87,5434,76554))
+
+# def info(**kwargs):  # keyword arguments
+#     return kwargs
+
+# print(info(name="Akarsh", age = 24 , profession = "Data scientist" ))
+
+
+
